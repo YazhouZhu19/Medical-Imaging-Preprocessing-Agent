634b5b98729f9eee8fc9255af143cdc67289e0ea
Yazhou Zhu <132653569+YazhouZhu19@users.noreply.github.com>
2025-04-20 14:31:49 +0800

    Create init.py

---
+++ datasets/init.py
@@ -0,0 +1,11 @@
+from .dataset import Dataset
+from .dataset_factory import DatasetFactory
+from .medical_decathlon import MedicalDecathlonDataset
+from .tcia import TCIADataset
+
+__all__ = [
+    'Dataset',
+    'DatasetFactory',
+    'MedicalDecathlonDataset',
+    'TCIADataset'
+]
